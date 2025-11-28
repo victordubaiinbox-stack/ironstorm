44668178d5bc4da619570e92b8cca5ebb19b3d47
Victor Petty <victor.dubai.inbox@gmail.com>
2025-11-28 16:53:24 -0500

    auto-update

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,19 @@
 @echo off
-echo Pushing update...
+REM Fire-and-forget Git updater for GitHub Pages
 
+echo Updating GitHub repository...
+
+REM Make sure we're in the directory of the batch file
+cd /d %~dp0
+
+REM Add all changes
 git add -A
+
+REM Commit with generic message
 git commit -m "auto-update"
+
+REM Push to main
 git push origin main
 
-echo Done!
+echo Update complete!
+pause
